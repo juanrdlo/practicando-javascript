672aa9c59e8719d825fb43797f4f4d7b3e15876c
Juan R. De Leon O <48454427+juanrdlo@users.noreply.github.com>
2021-07-26 21:32:55 -0500

    api openwhearther

--- assets/js/clima.js
+++ assets/js/clima.js
@@ -10,20 +10,52 @@ const pressurization = document.getElementById('pressurization');
 date.innerHTML = moment().format('dddd DD MMMM YYYY');
 
 // API
-const woeid = null;
-const latitude = '36.96';
-const longitude = '-122.02';
-const url = 'https://samples.openweathermap.org/data/2.5/weather?appid=d7c0002f10a680e17150bd2feb4402c9&'
+let latitude = '';
+let longitude = '';
+const url = 'https://api.openweathermap.org/data/2.5/weather?appid=d7c0002f10a680e17150bd2feb4402c9&units=metric&'
 
 function getWeather() {
     axios.get(`${url}lat=${latitude}&lon=${longitude}`)
-        .then(details => {
-            console.log(details)
+        .then(detailsWheather => {
+            const details = detailsWheather.data
+            temperature.innerHTML = details.main.temp.toFixed(0)
+            wind.innerHTML = details.wind.speed.toFixed(0)
+            humidity.innerHTML = details.main.humidity
+            visibility.innerHTML = details.visibility.toString().substr(0,3)
+            pressurization.innerHTML = details.main.pressure
         })
+        .catch(() => alert('Ha ocurrido un error. Por favor intentelo de nuevo'))
 }
 
 function getWeatherByLocation() {
-
+    navigator.permissions.query({name: 'geolocation'})
+        .then(result => {
+            switch (result.state) {
+                case 'granted':
+                case 'prompt':
+                    console.log('Todos los permisos fueron aprobados.')
+                    break;
+                case 'denied':
+                    alert('No podemos mostrar el tiempo, por que ha bloqueado los permisos.')
+                    break;
+            }
+            result.onchange = () => {
+                switch (result.state) {
+                    case 'granted':
+                    case 'prompt':
+                        console.log('Todos los permisos fueron aprobados.')
+                        break;
+                    case 'denied':
+                        alert('No podemos mostrar el tiempo, por que ha bloqueado los permisos.')
+                        break;
+                }
+            }
+        });
+    navigator.geolocation.getCurrentPosition(position => {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        getWeather()
+    });
 }
 
-getWeather()
+getWeatherByLocation()
